6e6162429e862c007c716e40595b1c0b5320edf7
kcheykim <kosalcheykim@gmail.com>
2021-12-08 14:29:14 -0800

    protecting the password

--- routes/api/user_routes.js
+++ routes/api/user_routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users - select all users fromt eh user table in the database and send it aback as JSON
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll()
+    User.findAll({
+            attributes: { exclude: ['password'] }
+        })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+            attributes: { exclude: ['password'] },
             where: {
                 id: req.params.id
             }
