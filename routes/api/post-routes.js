38dd6bfc57c7808a8ad8eae4aa7d02e966962caa
kcheykim <kosalcheykim@gmail.com>
2021-12-11 00:44:15 -0800

    defining the Post model

---
+++ routes/api/post-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        // Query configuration
+    })
+
+});
